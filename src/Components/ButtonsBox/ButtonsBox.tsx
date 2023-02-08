695e64da544bbbe3c9bb0130c1ee02755b0cc856
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2023-02-08 04:22:09 +0300

    fix code style, delete comments not used

--- src/Components/ButtonsBox/ButtonsBox.tsx
+++ src/Components/ButtonsBox/ButtonsBox.tsx
@@ -7,64 +7,6 @@ export const ButtonsBox: FC<SuperPropsType> = ({ id, state, setState }) => {
   const { count, maxValue, startValue, error, display } = state;
 
   const render = (id: idType, display: DisplayType) => {
-    /*
-    if (id === "counter") {
-      const onClickChangeCountHandler = () => {
-        switch (count) {
-          case maxValue:
-            break;
-          default:
-            setState({ ...state, count: count + 1 });
-        }
-      };
-      const onClickResetCountHandler = () => {
-        setState({ ...state, count: startValue });
-      };
-
-      return (
-        <>
-          <Button
-            isDisabled={error || maxValue === count ? true : false}
-            onClick={onClickChangeCountHandler}
-            text='inc'
-            key='inc'
-          />
-          <Button
-            isDisabled={error || startValue === count ? true : false}
-            onClick={onClickResetCountHandler}
-            text='reset'
-            key='reset'
-          />
-        </>
-      );
-    } else if (id === "settings") {
-      const onClickSetSettingsHandler = () => {
-        setState({ ...state, count: startValue, error: null });
-      };
-      return (
-        <Button
-          isDisabled={error === 'enter values and press "set"' ? false : true}
-          onClick={onClickSetSettingsHandler}
-          text='set'
-          key='set'
-        />
-      );
-    }  
-*/
-    // SECOND
-    /*
-    const onClickChangeCountHandler = () => {
-      switch (count) {
-        case maxValue:
-          break;
-        default:
-          setState({ ...state, count: count + 1 });
-      }
-    };
-    const onClickResetCountHandler = () => {
-      setState({ ...state, count: startValue });
-    };
-*/
     const onClickChangeCountHandler = () => {
       switch (count) {
         case maxValue:
